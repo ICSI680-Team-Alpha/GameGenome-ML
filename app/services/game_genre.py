985a4465996aa00e6a5e03276237e471c9b2ac9d
MoG Henry <84988222+MoGHenry@users.noreply.github.com>
2025-04-17 17:36:30 -0400

    Update game_genre.py

--- app/services/game_genre.py
+++ app/services/game_genre.py
@@ -1,9 +1,9 @@
 # app/services/genre_service.py
 
-from app.core.db import MongoDBSingleton
-from typing import List, Dict, Any
-from app.core.config import settings
 import pandas as pd
+from typing import List, Dict, Any
+from app.core.db import MongoDBSingleton
+from app.core.config import settings
 
 class GameGenre:
     _instance = None
@@ -14,6 +14,7 @@ class GameGenre:
             cls._instance = super(GameGenre, cls).__new__(cls)
         return cls._instance
     
+
     def get_genres(self, db_name: str = settings.DB_NAME, collection_name: str = "steam_genre") -> Dict[int, Dict[str, int]]:
         """Get and cache game genres."""
         if self._genre_cache is None:
@@ -25,6 +26,17 @@ class GameGenre:
             
             self._genre_cache = all_genres        
         return self._genre_cache
+    
+
+    def get_single_genre(self, appID: int, db_name: str = settings.DB_NAME, collection_name: str = "steam_genre") -> Dict[str, Any]:
+        """Get a single genre by ID."""
+        mongo = MongoDBSingleton()
+        database = mongo.get_database(db_name)
+        collection = database[collection_name]
+
+        genre = collection.find_one({"AppID": appID})
+        
+        return genre if genre else {}
     
     ## TODO : Add a method to convert Genre to DataFrame
     # def convert_genre_to_dataframe(self, genres: Dict[int, Dict[str, int]]) -> pd.DataFrame:
